8bba66d583cc2ab34a431b04d9396646d1c36fb9
unknown <christopher.wolf@citrix.com>
2019-03-06 13:24:52 -0500

    added authentication to router

--- src/router/index.js
+++ src/router/index.js
@@ -3,23 +3,41 @@ import Router from 'vue-router';
 
 Vue.use(Router);
 
-function route(path, view) {
+function route(path, view, meta = {}) {
   return {
     path,
     name: view,
+    meta,
     component: () => import(`@/views/${view}`),
   };
 }
 
-export default new Router({
+const router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
     route('/', 'Home'),
     route('/about', 'About'),
-    route('/register', 'Register'),
-    route('/login', 'Login'),
-    route('/leagues', 'Leagues'),
-    route('/calendar', 'Calendar'),
+    route('/register', 'Register', { authenticatedRedirect: '/leagues'}),
+    route('/login', 'Login', { authenticatedRedirect: '/leagues'}),
+    route('/leagues', 'Leagues', { requiresAuth: true }),
+    route('/calendar', 'Calendar', { requiresAuth: true }),
   ],
 });
+
+router.beforeEach((to, from, next) => {
+  const authenticated = require('../store').default.getters.AUTHENTICATED;
+  const requiresAuth = to.matched.some(record => record.meta.require);
+  console.log(authenticated)
+  if (requiresAuth && !authenticated) {
+    next("/");
+    return;
+  }
+  if (to.meta.authenticatedRedirect && authenticated) {
+    next(to.meta.authenticatedRedirect);
+    return;
+  }
+  next();
+})
+
+export default router;
--- src/views/Leagues.vue
+++ src/views/Leagues.vue
@@ -4,17 +4,19 @@
       <v-layout align-center justify-center>
         <v-flex xs12 sm8 md4 lg3>
           <v-layout wrap>
-            <!--<v-card class="elevation-12" align-center justify-center>
-              <v-btn fab dark color="primary">
-                <v-icon dark>add</v-icon>
-              </v-btn>
-              <v-flex>Add league</v-flex>
-            </v-card>
-            <v-card class="elevation-12">
-              <v-btn fab dark color="primary">
-                <v-icon dark>add</v-icon>
-              </v-btn>
-            </v-card> -->
+            <v-card to="/login" hover ripple img="https://cdn.vuetifyjs.com/images/cards/docks.jpg">
+        <v-card-title>
+          <div>
+            <span class="grey--text">Number 10</span><br>
+            <span>Whitehaven Beach</span><br>
+            <span>Whitsunday Island, Whitsunday Islands</span>
+          </div>
+        </v-card-title>
+        <v-card-actions>
+          <v-btn flat color="orange">Share</v-btn>
+          <v-btn flat color="orange">Explore</v-btn>
+        </v-card-actions>
+      </v-card>
             <div>
               <v-btn large>+<br/>Add league</v-btn>
             </div>
