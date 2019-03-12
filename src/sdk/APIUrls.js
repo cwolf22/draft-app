5f2e5c34a91aaced21ccc8b170823b1289c3b839
unknown <christopher.wolf@citrix.com>
2019-03-11 20:28:21 -0400

    updates for deploy

--- src/sdk/APIUrls.js
+++ src/sdk/APIUrls.js
@@ -1,7 +1,9 @@
 const isDev = typeof process !== 'undefined' ? process.env.NODE_ENV === 'development' : false;
 
-const API_BASE = isDev ? 'http://localhost:3030/bs/v1/drafter' : '/bs/v1/drafter';
+const API_BASE = isDev ? 'http://localhost:3000/bs/v1/drafter' : 'https://api.baitingsheep.com/bs/v1/drafter';
 
+console.log(`ENV: ${process.env.NODE_ENV }`);
+console.log(`API BASE: ${API_BASE }`);
 const API = {
   REGISTER_USER: () => `${API_BASE}/register`,
   LOGIN_USER: () => `${API_BASE}/login`,
